0b4155e5e4c4ba53e3688651944100f6435e6d5b
luisoscategui <luis.oscategui@xurface.com>
2015-06-12 09:29:49 -0500

    project initial

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('inscripcionesApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
